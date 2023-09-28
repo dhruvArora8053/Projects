db64ed1f169e60c9b9474ec186c51f54a04daa66
Dhruv Arora <dhruvarora.code@gmail.com>
2023-09-28 17:50:24 +0530

    implemented highscore

--- 05-Guess-My-Number/final/script.js
+++ 05-Guess-My-Number/final/script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -19,6 +20,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is too high
   } else if (guess > secretNumber) {
@@ -45,9 +51,10 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.guess').value = null;
-  document.querySelector('.score').textContent = 0;
+  score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
   document.querySelector('.message').textContent = 'Start Guessing...';
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').textContent = '?';
